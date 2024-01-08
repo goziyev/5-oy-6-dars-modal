dbea3d764a20358bd7f868beaf3914ee8710b68b
goziyev <goziyevmuhammadgozi@gmail.com>
2024-01-08 13:07:16 +0500

    task completed

--- script/script.js
+++ script/script.js
@@ -1,18 +1,84 @@
-const btn = document.getElementById('btn');
-const modal = document.querySelector('.modal');
-const accpect = document.getElementById('accpect')
-const declined = document.getElementById('declined')
-const modalHtml = document.getElementById('main_modal')
-btn && btn.addEventListener("click",function(){
-    modalHtml.setAttribute("class","html");
-    modal.setAttribute("class","modal modal-btn")
-})
-function modalFunction(){
-    modalHtml.setAttribute('class','html1')
-    modal.setAttribute("class","modal")
-} 
-modalHtml && modalHtml.addEventListener("click",modalFunction)
-accpect && accpect.addEventListener("click",modalFunction)
-declined && declined.addEventListener("click",modalFunction)
+const btn = document.getElementById("btn");
+const modal = document.querySelector(".modal");
+const accpect = document.getElementById("accpect");
+const declined = document.getElementById("declined");
+const modalHtml = document.getElementById("main_modal");
+btn &&
+  btn.addEventListener("click", function () {
+    modalHtml.setAttribute("class", "html");
+    modal.setAttribute("class", "modal modal-btn");
+  });
+function modalFunction() {
+  modalHtml.setAttribute("class", "html1");
+  modal.setAttribute("class", "modal");
+}
+modalHtml && modalHtml.addEventListener("click", modalFunction);
+accpect && accpect.addEventListener("click", function(){
+  setTimeout(modalFunction,1500)
+});
+declined && declined.addEventListener("click", modalFunction);
 
 
+
+
+const sound = document.getElementById("sound");
+const triggers = document.querySelectorAll(".js-confetti");
+const defaults = {
+  disableForReducedMotion: true
+};
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(200 * particleRatio)
+    })
+  );
+}
+
+function confettiExplosion(origin) {
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+    origin
+  });
+  fire(0.2, {
+    spread: 60,
+    origin
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    origin
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    origin
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+    origin
+  });
+}
+
+Array.from(triggers).forEach((trigger) => {
+  trigger.addEventListener("click", () => {
+    const rect = trigger.getBoundingClientRect();
+    const center = {
+      x: rect.left + rect.width / 2,
+      y: rect.top + rect.height / 2
+    };
+    const origin = {
+      x: center.x / window.innerWidth,
+      y: center.y / window.innerHeight
+    };
+
+    if (sound) {
+      sound.currentTime = 0;
+      sound.play();
+    }
+    confettiExplosion(origin);
+  });
+});
